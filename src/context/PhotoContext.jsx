386013d176f9fad34547cf0cf01cd65c68f449b8
jcorp11 <joaco.011@gmail.com>
2023-12-23 18:29:11 -0300

    changes

--- src/context/PhotoContext.jsx
+++ src/context/PhotoContext.jsx
@@ -5,7 +5,7 @@ const PhotoProvider = ({ children }) => {
   const [data, setData] = useState(null);
   const isProduction = process.env.NODE_ENV === "production";
   const PHOTO_URL = isProduction ? "/photos.json" : "/photos.json";
-  console.log(PHOTO_URL);
+  // console.log(PHOTO_URL);
   useEffect(() => {
     // Make a GET request using Axios
     axios
